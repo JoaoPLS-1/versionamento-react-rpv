76e3d315eef324c83fb2bc65bd6880bffc91576a
JoaoPLS-1 <barrancoij@gmail.com>
2026-04-14 21:40:27 -0300

    feat: altera tema pelo botão

--- src/pages/exercicio-1/index.tsx
+++ src/pages/exercicio-1/index.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react"
+
 export default function Ex01() {
+    const [mudaCor, setMudaCor] = useState(false)
 
     return(
         <>
-            <h1>Exercicio 01</h1>
+            <div className={`w-full h-screen ${ mudaCor ? 'bg-red-700 ': 'bg-blue-300'} flex flex-col items-center justify-center gap-5`}>
+                <h1 className="text-4xl">Exercicio 1</h1>
+                <button className={`p-4  rounded-2xl  bg-black cursor-pointer`} onClick={() => setMudaCor(!mudaCor)} >Alterar cor</button>
+            </div>
         </>
     )
 }
