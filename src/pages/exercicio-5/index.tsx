a85b68a37051aa8660181562029667a8f22b5b1f
JoaoPLS-1 <barrancoij@gmail.com>
2026-04-16 20:09:22 -0300

    feat: consumindo api jsonplaceholder e renderizando em tela

--- src/pages/exercicio-5/index.tsx
+++ src/pages/exercicio-5/index.tsx
@@ -1,8 +1,45 @@
-export default function Ex01() {
+import { useState } from "react";
+import { useEffect } from "react";
 
-    return(
+interface IUsersFetch {
+    id: number,
+    name: string,
+    username: string,
+    email: string
+}
+export default function Ex05() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        async function buscarUsuarios() {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+            const data = await response.json();
+            setUsers(data);
+        }
+        buscarUsuarios();
+
+    }, []);
+    console.log("users:", users)
+    return (
         <>
-            <h1>Exercicio 01</h1>
+            <div className="w-full h-screen flex flex-col bg-amber-700 items-center justify-center">
+                <h1 className="text-3xl">Exercicio 4</h1>
+                <p>{users.map((usuarios: IUsersFetch) => {
+                    return (
+                       
+                        
+                            <div key={usuarios.id} className="grid grid-cols-3 flex flex-col gap-2 border border-zinc-300">
+
+                                <p><strong>Nome:</strong> {usuarios.name}</p>
+                                <p><strong>username:</strong> {usuarios.username}</p>
+                                <p><strong>email:</strong> {usuarios.email}</p>
+
+                            </div>
+                    
+
+                    )
+                })}</p>
+
+            </div>
         </>
     )
 }
