ff0e6f5ce15a786cd0581206165b3803c6d54397
JoaoPLS-1 <barrancoij@gmail.com>
2026-04-15 22:20:25 -0300

    feat: validacao avançada - implementando components input e inputMask

--- src/pages/exercicio-4/index.tsx
+++ src/pages/exercicio-4/index.tsx
@@ -1,8 +1,110 @@
-export default function Ex01() {
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { InputMask } from "./InputMask"
+import { Input } from "./Input"
 
-    return(
+
+
+const validacao = z.object({
+    nome: z.string().min(1, "Campo obrigatório."),
+    email: z.email("Email inválido"),
+    endereco: z.string().min(1, "Campo obrigatório."),
+    bairro: z.string().min(1, "Campo obrigatório."),
+    cep: z.string().min(1, "Campo obrigatório."),
+    cpf: z.string().min(1, "Campo obrigatório."),
+
+
+})
+
+export type FormType = z.infer<typeof validacao>
+
+export default function Ex04() {
+
+
+    const formulario = useForm<FormType>({
+        resolver: zodResolver(validacao)
+    })
+
+    function submeterFormulario(camposDoFormulario: FormType) {
+        formulario.reset();
+        console.log('Campos:', camposDoFormulario)
+    }
+    return (
         <>
-            <h1>Exercicio 01</h1>
+            <div className="w-full h-screen bg-pink-300 flex items-center justify-center flex-col gap-8">
+                <h1 className="text-4xl">Exercicio 04</h1>
+
+                <div className="flex">
+                    <form onSubmit={formulario.handleSubmit(submeterFormulario)} className="flex flex-col gap-4">
+                        <div className="flex gap-5">
+                            <div>
+                                <InputMask
+                                    errors={formulario.formState.errors}
+                                    label='CPF'
+                                    masks='cpf'
+                                    name='cpf'
+                                    register={formulario.register}
+                                    required
+                                    size={9}
+                                    placeholder='Digite o seu CPF ou CNPJ'
+                                />
+                                <Input
+                                    errors={formulario.formState.errors}
+                                    label='Nome'
+                                    name='nome'
+                                    register={formulario.register}
+                                    required
+                                    size={9}
+                                />
+                                <Input
+                                    errors={formulario.formState.errors}
+                                    label='Email'
+                                    name='email'
+                                    register={formulario.register}
+                                    required
+                                    size={9}
+                                />
+                            </div>
+
+                            <div>
+                                <InputMask
+                                    errors={formulario.formState.errors}
+                                    label='CEP'
+                                    masks='cep'
+                                    name='cep'
+                                    register={formulario.register}
+                                    required
+                                    size={9}
+                                    placeholder='Digite o seu CEP'
+                                />
+                                <Input
+                                    errors={formulario.formState.errors}
+                                    label='Endereco'
+                                    name='endereco'
+                                    register={formulario.register}
+                                    required
+                                    size={9}
+                                />
+                                <Input
+                                    errors={formulario.formState.errors}
+                                    label='Bairro'
+                                    name='bairro'
+                                    register={formulario.register}
+                                    required
+                                    size={9}
+                                />
+                            </div>
+                        </div>
+
+
+                        <div className="flex justify-center mt-2">
+                            <button type="submit" className="bg-black rounded-md py-2 mt-6 min-w-full cursor-pointer">Enviar</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div >
         </>
     )
 }
---
+++ src/pages/exercicio-4/utils/index.tsx
@@ -0,0 +1,8 @@
+export const masksDefault = {
+    cpf: ["###.###.###-##"],
+    telefone: "(##) ####-####",
+    celular: "(##) #####-####",
+    cep: "#####-###"
+}
+
+export type TMasksDefault = keyof typeof masksDefault
