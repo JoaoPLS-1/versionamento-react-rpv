d6a773dab34725a9e1cc4709d168347e1bcb66df
JoaoPLS-1 <barrancoij@gmail.com>
2026-04-16 21:46:45 -0300

    feat: desenvolvendo o exercicio

--- src/pages/exercicio-6/index.tsx
+++ src/pages/exercicio-6/index.tsx
@@ -1,8 +1,22 @@
-export default function Ex01() {
-
+import { useState } from "react";
+import { useEffect } from "react";
+export default function Ex06() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        async function buscaUsuarios() {
+            const response = await fetch ("https://jsonplaceholder.typicode.com/users")
+            const data = await response.json();
+            setUsers(data);
+        }
+        buscaUsuarios();
+    }, [])
     return(
         <>
-            <h1>Exercicio 01</h1>
+        <div className="w-full h-screen bg-violet-950 flex flex-col items-center justify-center">
+
+            <h1 className="text-3xl">Exercicio 06</h1>
+            <input type="text" placeholder="Digite algo..." className="bg-black rounded-md"  />
+        </div>
         </>
     )
 }
